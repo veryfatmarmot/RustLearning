f30c5d042755cc0bc6a7220b3e5e0dffa255a9eb
Sergii Bondar <bondarjunk@gmail.com>
2025-12-05 21:13:24 +0000

    Ready to publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,54 @@
-use std::{
-    error::Error, 
-    fs, 
-    env
-};
+use std::error::Error;
 
+/// Holds parameters of a rectangle in integer representation.
+///
+/// # Examples
+///
+/// ```
+/// use sergii_bondar_dummy_crate::Rectangle;
+///
+/// let rec = Rectangle { width: 10, height: 1 };
+/// assert_eq!(10, rec.width);
+/// assert_eq!(1, rec.height);
+/// ```
+#[derive(Debug)]
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let mut list = [
+        Rectangle {
+            width: 10,
+            height: 1,
+        },
+        Rectangle {
+            width: 3,
+            height: 5,
+        },
+        Rectangle {
+            width: 7,
+            height: 12,
+        },
+    ];
 
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
+    let mut sort_operations = vec![];
+    let value = String::from("closure called");
 
-    println!("The rusult:");
-    for (index, line) in results.iter().enumerate() {
-        println!("{index}. {line}");
-    }    
-    println!("\n");
+    list.sort_by_key(|r| {
+        sort_operations.push(value.clone());
+        r.width
+    });
+    println!("{list:#?}");
 
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) ->Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
-}
-
-pub fn search_case_insensitive<'a>(
-    query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-
-    result
-}
-
 // ==================================================================================================================
-
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-    pub ignore_case: bool,
-}
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Problem parsing arguments: not enough arguments");
-        }
-
-        Ok(Config { 
-            query: args[1].clone(), 
-            file_path: args[2].clone(),
-            ignore_case: env::var("IGNORE_CASE").is_ok(),
-        })
-    }
-}
-
-// ==================================================================================================================
-
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn case_sensitive () {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive () {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
-}
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-use std::{env, error::Error};
-use minigrep::Config;
-
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    //Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args)?;
-
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.file_path);
-
-    minigrep::run(config)
+    sergii_bondar_dummy_crate::run()
 }
