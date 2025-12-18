ab49b666c1575876598a52d297216175102cd48d
Sergii Bondar <bondarjunk@gmail.com>
2025-12-18 15:37:14 +0000

    Concurrency Timeout async wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
-use std::error::Error;
+extern crate trpl; // required for mdbook test
 
-fn main() -> Result<(), Box<dyn Error>> {
-    sergii_bondar_dummy_crate::run()
+use std::{future::Future, time::Duration};
+
+mod scope_time_logger;
+use scope_time_logger::ScopeTimeLogger;
+use trpl::Either;
+
+fn main() {
+    trpl::run(async {
+        let _main_scope = ScopeTimeLogger::new("Main");
+
+        let slow = async {
+            trpl::sleep(Duration::from_millis(1000)).await;
+            "I finished!"
+        };
+
+        match timeout(slow, Duration::from_millis(555)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs_f32())
+            }
+        }
+    });
+}
+
+
+async fn timeout<'a, F>(
+    future: F,
+    timeout_dur: Duration,
+) -> Result<&'a str, Duration> 
+where
+    F: Future<Output = &'a str>,
+{
+    let timeout_future = async {
+        trpl::sleep(timeout_dur).await;
+        timeout_dur
+    };
+
+    match trpl::race(future, timeout_future).await {
+        Either::Left(res) => Ok(res),
+        Either::Right(res) => Err(res),
+    }
 }
