322d780a3256aa058890c44f80c2bc167b3a954a
Sergii Bondar <bondarjunk@gmail.com>
2025-12-25 14:34:17 +0000

    Enum-based blog post with states

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,101 @@
-extern crate trpl; // required for mdbook test
-
-use std::{pin::pin, time::Duration};
-use trpl::{ReceiverStream, Stream, StreamExt};
-
-//use futures::stream::FuturesUnordered;
-
-mod scope_time_logger;
-use scope_time_logger::ScopeTimeLogger;
-
 fn main() {
-    let _scope_time = ScopeTimeLogger::new("Main");
+    let mut post = Post::new();
 
-    trpl::run(async {
-        let messages = get_messages().timeout(Duration::from_millis(200));
-        let intervals = get_intervals()
-            .map(|count| format!("Interval: {count}"))
-            .throttle(Duration::from_millis(100))
-            .timeout(Duration::from_secs(10));
-        let merged = messages.merge(intervals).take(40);
-        let mut stream = pin!(merged);
+    post.add_text("I ate a salad for lunch today");
+    post.add_text(" and some pickles.");
+    post.approve();
+    assert_eq!("", post.content());
 
-        while let Some(result) = stream.next().await {
-            match result {
-                Ok(message) => println!("{message}"),
-                Err(reason) => eprintln!("Problem: {reason:?}"),
-            }
-        }
-    });
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    println!("{}", post.content());
 }
 
-fn get_messages() -> impl Stream<Item = String> {
-    let (tx, rx) = trpl::channel();
+// ==========================================================================================================
 
-    trpl::spawn_task(async move {
-        let messages = ('a'..='j').map(|i| i.to_string());
-
-        for (index, message) in messages.into_iter().enumerate() {
-            let time_to_sleep = if index % 2 == 0 {
-                Duration::from_millis(50)
-            } else {
-                Duration::from_millis(150)
-            };
-            trpl::sleep(time_to_sleep).await;
-            
-            if let Err(send_error) = tx.send(format!("Message: '{message}'")) {
-                eprintln!("Cannot send message '{message}': {send_error}");
-                break;
-            }
-        }
-    });
-
-    ReceiverStream::new(rx)
+enum Post {
+    Draft(PostDraft),
+    PendingReview(PostPendingReview),
+    Published(PostPublished),
 }
 
-fn get_intervals() -> impl Stream<Item = u32> {
-    let (tx, rx) = trpl::channel();
+impl Post {
+    fn new() -> Post {
+        Post::Draft(PostDraft::new())
+    }
 
-    trpl::spawn_task(async move {
-        let mut count = 0;
-        loop {
-            trpl::sleep(Duration::from_millis(1)).await;
-            count += 1;
-
-            if let Err(send_error) = tx.send(count) {
-                eprintln!("Could not send interval {count}: {send_error}");
-                break;
-            };
+    fn add_text(&mut self, text: &str) {
+        if let Post::Draft(draft) = self {
+            draft.add_text(text);
         }
-    });
+    }
 
-    ReceiverStream::new(rx)
+    fn request_review(&mut self) {
+        if let Post::Draft(state_obj) = self {
+            let content = std::mem::take(&mut state_obj.content);
+            *self = Post::PendingReview(PostPendingReview::new(content));
+        }
+    }
+
+    fn approve(&mut self) {
+        if let Post::PendingReview(state_obj) = self {
+            let content = std::mem::take(&mut state_obj.content);
+            *self = Post::Published(PostPublished::new(content));
+        }
+    }
+
+    fn content(&self) -> &str {
+        match self {
+            Post::Published(published) => published.content(),
+            _ => "",
+        }
+    }
+}
+
+// ==========================================================================================================
+
+struct PostDraft {
+    content: String,
+}
+
+impl PostDraft {
+    fn new() -> PostDraft {
+        PostDraft {
+            content: String::new(),
+        }
+    }
+
+    fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+}
+
+// ==========================================================================================================
+
+struct PostPendingReview {
+    content: String,
+}
+
+impl PostPendingReview {
+    fn new(content: String) -> PostPendingReview {
+        PostPendingReview { content: content }
+    }
+}
+
+// ==========================================================================================================
+
+struct PostPublished {
+    content: String,
+}
+
+impl PostPublished {
+    fn new(content: String) -> PostPublished {
+        PostPublished { content: content }
+    }
+
+    fn content(&self) -> &str {
+        &self.content
+    }
 }
