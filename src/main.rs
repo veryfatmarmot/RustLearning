a5633277a5aec52fd73cd06f9f0a36c91432d566
Sergii Bondar <bondarjunk@gmail.com>
2026-01-04 09:30:51 +0000

    Clean before web server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use hello_macro::HelloMacro;
-use hello_macro_derive::HelloMacro;
-
-#[derive(HelloMacro)]
-struct Pancakes {
-    marmot: String,
-    foo: i32,
-}
 
 fn main() {
-    Pancakes::hello_macro();
 }
